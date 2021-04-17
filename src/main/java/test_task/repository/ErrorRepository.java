975899ccb76674c88b9e3459f6e252eaf3434771
FormAToz <kycok2@yandex.ru>
2021-04-17 19:37:02 +0300

    Логика в сервисе ErrorService и ErrorRepository реализована.

--- src/main/java/test_task/repository/ErrorRepository.java
+++ src/main/java/test_task/repository/ErrorRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import test_task.model.Error;
 
+import java.util.Optional;
+
 @Repository
 public interface ErrorRepository extends JpaRepository<Error, Long> {
+    Optional<Error> findFirstByOrderByCreatedDesc();
 }
--- src/main/java/test_task/service/ErrorService.java
+++ src/main/java/test_task/service/ErrorService.java
@@ -1,11 +1,23 @@
 package test_task.service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import test_task.model.Error;
+import test_task.repository.ErrorRepository;
 
+@RequiredArgsConstructor
 @Service
 public class ErrorService {
+
+    private final ErrorRepository errorRepository;
+
+    /**
+     * Метод получения последней ошибки
+     * @return объект класса Error
+     * @throws IllegalStateException в случае, если ни одной ошибки не найдено в базе данных
+     */
     public Error getLastError() {
-        return null;
+        return errorRepository.findFirstByOrderByCreatedDesc()
+                .orElseThrow(() -> new IllegalStateException("Невозможно получить последнюю ошибку"));
     }
 }
